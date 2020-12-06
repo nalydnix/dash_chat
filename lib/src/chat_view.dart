7653bde7501e443af1566d4e0210a7c312798bc8
nalydnix <dylanxin@users.noreply.github.com>
2020-12-06 14:25:02 +1300

    Align chat input bar to bottom

--- lib/src/chat_view.dart
+++ lib/src/chat_view.dart
@@ -481,42 +481,43 @@ class DashChatState extends State<DashChat> {
                     : MainAxisAlignment.end,
                 children: <Widget>[
                   MessageListView(
-                    avatarMaxSize: widget.avatarMaxSize,
-                    messagePadding: widget.messagePadding,
-                    constraints: constraints,
-                    shouldShowLoadEarlier: widget.shouldShowLoadEarlier,
-                    showLoadEarlierWidget: widget.showLoadEarlierWidget,
-                    onLoadEarlier: widget.onLoadEarlier,
-                    defaultLoadCallback: changeDefaultLoadMore,
-                    messageContainerPadding: widget.messageContainerPadding,
-                    scrollController: widget.scrollController != null
-                        ? widget.scrollController
-                        : scrollController,
-                    user: widget.user,
-                    messages: widget.messages,
-                    showuserAvatar: widget.showUserAvatar,
-                    dateFormat: widget.dateFormat,
-                    timeFormat: widget.timeFormat,
-                    inverted: widget.inverted,
-                    showAvatarForEverMessage: widget.showAvatarForEveryMessage,
-                    onLongPressAvatar: widget.onLongPressAvatar,
-                    onPressAvatar: widget.onPressAvatar,
-                    onLongPressMessage: widget.onLongPressMessage,
-                    avatarBuilder: widget.avatarBuilder,
-                    messageBuilder: widget.messageBuilder,
-                    messageTextBuilder: widget.messageTextBuilder,
-                    messageImageBuilder: widget.messageImageBuilder,
-                    messageTimeBuilder: widget.messageTimeBuilder,
-                    dateBuilder: widget.dateBuilder,
-                    messageContainerDecoration:
-                        widget.messageContainerDecoration,
-                    parsePatterns: widget.parsePatterns,
-                    changeVisible: changeVisible,
-                    visible: visible,
-                    showLoadMore: showLoadMore,
-                    messageButtonsBuilder: widget.messageButtonsBuilder,
-                    messageDecorationBuilder: widget.messageDecorationBuilder
-                  ),
+                      avatarMaxSize: widget.avatarMaxSize,
+                      messagePadding: widget.messagePadding,
+                      constraints: constraints,
+                      shouldShowLoadEarlier: widget.shouldShowLoadEarlier,
+                      showLoadEarlierWidget: widget.showLoadEarlierWidget,
+                      onLoadEarlier: widget.onLoadEarlier,
+                      defaultLoadCallback: changeDefaultLoadMore,
+                      messageContainerPadding: widget.messageContainerPadding,
+                      scrollController: widget.scrollController != null
+                          ? widget.scrollController
+                          : scrollController,
+                      user: widget.user,
+                      messages: widget.messages,
+                      showuserAvatar: widget.showUserAvatar,
+                      dateFormat: widget.dateFormat,
+                      timeFormat: widget.timeFormat,
+                      inverted: widget.inverted,
+                      showAvatarForEverMessage:
+                          widget.showAvatarForEveryMessage,
+                      onLongPressAvatar: widget.onLongPressAvatar,
+                      onPressAvatar: widget.onPressAvatar,
+                      onLongPressMessage: widget.onLongPressMessage,
+                      avatarBuilder: widget.avatarBuilder,
+                      messageBuilder: widget.messageBuilder,
+                      messageTextBuilder: widget.messageTextBuilder,
+                      messageImageBuilder: widget.messageImageBuilder,
+                      messageTimeBuilder: widget.messageTimeBuilder,
+                      dateBuilder: widget.dateBuilder,
+                      messageContainerDecoration:
+                          widget.messageContainerDecoration,
+                      parsePatterns: widget.parsePatterns,
+                      changeVisible: changeVisible,
+                      visible: visible,
+                      showLoadMore: showLoadMore,
+                      messageButtonsBuilder: widget.messageButtonsBuilder,
+                      messageDecorationBuilder:
+                          widget.messageDecorationBuilder),
                   if (widget.messages.length != 0 &&
                       widget.messages.last.user.uid != widget.user.uid &&
                       widget.messages.last.quickReplies != null)
@@ -551,42 +552,46 @@ class DashChatState extends State<DashChat> {
                   if (widget.chatFooterBuilder != null)
                     widget.chatFooterBuilder(),
                   if (!widget.readOnly)
-                    ChatInputToolbar(
-                        key: inputKey,
-                        sendOnEnter: widget.sendOnEnter,
-                        textInputAction: widget.textInputAction,
-                        inputToolbarPadding: widget.inputToolbarPadding,
-                        textDirection: widget.inputTextDirection,
-                        inputToolbarMargin: widget.inputToolbarMargin,
-                        showTraillingBeforeSend: widget.showTraillingBeforeSend,
-                        inputMaxLines: widget.inputMaxLines,
-                        controller: textController,
-                        inputDecoration: widget.inputDecoration,
-                        textCapitalization: widget.textCapitalization,
-                        onSend: widget.onSend,
-                        user: widget.user,
-                        messageIdGenerator: widget.messageIdGenerator,
-                        maxInputLength: widget.maxInputLength,
-                        sendButtonBuilder: widget.sendButtonBuilder,
-                        text: widget.text != null ? widget.text : _text,
-                        onTextChange: widget.onTextChange != null
-                            ? widget.onTextChange
-                            : onTextChange,
-                        inputDisabled: widget.inputDisabled,
-                        leading: widget.leading,
-                        trailling: widget.trailing,
-                        inputContainerStyle: widget.inputContainerStyle,
-                        inputTextStyle: widget.inputTextStyle,
-                        inputFooterBuilder: widget.inputFooterBuilder,
-                        inputCursorColor: widget.inputCursorColor,
-                        inputCursorWidth: widget.inputCursorWidth,
-                        showInputCursor: widget.showInputCursor,
-                        alwaysShowSend: widget.alwaysShowSend,
-                        scrollController: widget.scrollController != null
-                            ? widget.scrollController
-                            : scrollController,
-                        focusNode: inputFocusNode,
-                        reverse: widget.inverted)
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: ChatInputToolbar(
+                          key: inputKey,
+                          sendOnEnter: widget.sendOnEnter,
+                          textInputAction: widget.textInputAction,
+                          inputToolbarPadding: widget.inputToolbarPadding,
+                          textDirection: widget.inputTextDirection,
+                          inputToolbarMargin: widget.inputToolbarMargin,
+                          showTraillingBeforeSend:
+                              widget.showTraillingBeforeSend,
+                          inputMaxLines: widget.inputMaxLines,
+                          controller: textController,
+                          inputDecoration: widget.inputDecoration,
+                          textCapitalization: widget.textCapitalization,
+                          onSend: widget.onSend,
+                          user: widget.user,
+                          messageIdGenerator: widget.messageIdGenerator,
+                          maxInputLength: widget.maxInputLength,
+                          sendButtonBuilder: widget.sendButtonBuilder,
+                          text: widget.text != null ? widget.text : _text,
+                          onTextChange: widget.onTextChange != null
+                              ? widget.onTextChange
+                              : onTextChange,
+                          inputDisabled: widget.inputDisabled,
+                          leading: widget.leading,
+                          trailling: widget.trailing,
+                          inputContainerStyle: widget.inputContainerStyle,
+                          inputTextStyle: widget.inputTextStyle,
+                          inputFooterBuilder: widget.inputFooterBuilder,
+                          inputCursorColor: widget.inputCursorColor,
+                          inputCursorWidth: widget.inputCursorWidth,
+                          showInputCursor: widget.showInputCursor,
+                          alwaysShowSend: widget.alwaysShowSend,
+                          scrollController: widget.scrollController != null
+                              ? widget.scrollController
+                              : scrollController,
+                          focusNode: inputFocusNode,
+                          reverse: widget.inverted),
+                    )
                 ],
               ),
               if (visible && !_initialLoad)
